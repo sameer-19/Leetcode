3d03815fead0855e0e70c75a78ee9518f9715be5
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-03-13 09:01:01 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.05%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.length(),i,f=0;
+        for(i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else
+            {
+                if(st.empty()) { f=1;break;}
+                if(s[i]==')' && st.top()=='(') st.pop();
+                else if(s[i]=='}' && st.top()=='{') st.pop();
+                else if(s[i]==']' && st.top()=='[') st.pop();
+                else {f=1;break;}
+            }
+        }
+        if(!st.empty() || f==1) return false;
+        return true;
+    }
+};
