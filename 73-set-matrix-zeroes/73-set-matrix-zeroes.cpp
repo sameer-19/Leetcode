235d1d1f4e457b583bc67246d5f0a06ea116b56f
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-03 11:28:02 +0530

    Time: 32 ms (11.71%), Space: 13.3 MB (19.57%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        set<int> row,col;
+        vector<int> row,col;
         int m=mat.size(),n=mat[0].size();
         int i,j;
         for(i=0;i<m;i++)
         {
             for(j=0;j<n;j++)
             {
-                if(mat[i][j]==0) {row.insert(i);col.insert(j);}
+                if(mat[i][j]==0) {row.push_back(i);col.push_back(j);}
             }
         }
         for(auto x: row)
