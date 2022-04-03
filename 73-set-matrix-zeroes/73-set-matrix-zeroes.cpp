e1e5493286458e1d42641dccd497c0640cf7f3d1
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-03 11:50:00 +0530

    Time: 16 ms (73.78%), Space: 13.3 MB (54.31%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,22 +3,28 @@ public:
     void setZeroes(vector<vector<int>>& mat) {
         vector<int> row,col;
         int m=mat.size(),n=mat[0].size();
-        int i,j;
+        int i,j,col0=1;
         for(i=0;i<m;i++)
         {
-            for(j=0;j<n;j++)
+            if(mat[i][0]==0) col0=0;
+            for(j=1;j<n;j++)   
             {
-                if(mat[i][j]==0) {row.push_back(i);col.push_back(j);}
+                if(mat[i][j]==0)
+                {
+                    mat[i][0]=mat[0][j]=0;
+                }
             }
         }
-        for(auto x: row)
+        
+        for(i=m-1;i>=0;i--)
         {
-            for(j=0;j<n;j++) mat[x][j]=0;
+            for(j=n-1;j>=1;j--)
+            {
+                if(mat[i][0]==0 || mat[0][j]==0) mat[i][j]=0;
+            }
+            if(col0==0) mat[i][0]=0;
         }
-        for(auto x: col) 
-        {
-            for(i=0;i<m;i++) mat[i][x]=0;
-        }
+        
         return;
     }
 };
