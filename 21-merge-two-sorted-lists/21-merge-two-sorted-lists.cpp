d4d3167cc728ce1fe653b1650f8252b8972970f2
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-06 12:00:46 +0530

    Time: 7 ms (83.29%), Space: 15 MB (14.92%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -42,32 +42,6 @@ public:
     }
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *ans=new ListNode();
-        ListNode* tmp=ans;
         return solve(ans,l1,l2);
-        // vector<int> v;
-        // ListNode *tmp;
-        // tmp=l1;
-        // while(tmp!=NULL)
-        // {
-        //     v.push_back(tmp->val);
-        //     tmp=tmp->next;
-        // }
-        // tmp=l2;
-        // while(tmp!=NULL)
-        // {
-        //     v.push_back(tmp->val);
-        //     tmp=tmp->next;
-        // }
-        // if(v.size()==0) return l1;
-        // sort(v.begin(),v.end());
-        // ListNode *ans=new ListNode(v[0]);
-        // tmp=ans;
-        // for(int i=1;i<size(v);i++)
-        // {
-        //     ListNode *z=new ListNode(v[i]);
-        //     ans->next=z;
-        //     ans=ans->next;
-        // }
-        // return tmp;
     }
 };
