7988706554a2d6c66abdff4b9c8ec4582ce02947
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-06 22:29:50 +0530

    Time: 10 ms (60.65%), Space: 7.6 MB (74.63%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -10,10 +10,13 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         if(head==NULL || head->next==NULL) return NULL;
+        
         ListNode *slow = head, *fast = head;
+        
         while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
+            
             if (slow == fast) 
             {
                 while(head!=slow)
@@ -24,6 +27,7 @@ public:
                 return head;
             }
         }
+        
         return NULL;
     }
 };
