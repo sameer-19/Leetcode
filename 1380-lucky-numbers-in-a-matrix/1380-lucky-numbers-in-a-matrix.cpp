9b0704a1887f98b3dc54cd42a850771c5d53d8a1
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-25 06:06:12 +0530

    Time: 27 ms (59.47%), Space: 11.6 MB (30.66%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -8,55 +8,53 @@ public:
         
         int m=mat.size(),n=mat[0].size();
         
-        for(i=0;i<m;i++)
-        {   
-            for(j=0;j<n;j++)
-            {
-                int mn=INT_MAX,mx=INT_MIN;
-                
-                for(k=0;k<n;k++)
-                {
-                    mn=min(mat[i][k],mn);
-                }
-                
-                for(k=0;k<m;k++)
-                {
-                    mx=max(mat[k][j],mx);
-                }
-                
-                if(mn==mx) ans.push_back(mn);
-            }
-        }
-        
 //         for(i=0;i<m;i++)
-//         {
-//             int mn=INT_MAX,id1=i,id2=0;
+//         {   
 //             for(j=0;j<n;j++)
 //             {
-//                 if(mn>mat[i][j])
+//                 int mn=INT_MAX,mx=INT_MIN;
+                
+//                 for(k=0;k<n;k++)
 //                 {
-//                     id2=j;
+//                     mn=min(mat[i][k],mn);
 //                 }
-//                 mn=min(mat[i][j],mn);
+                
+//                 for(k=0;k<m;k++)
+//                 {
+//                     mx=max(mat[k][j],mx);
+//                 }
+                
+//                 if(mn==mx) ans.push_back(mn);
 //             }
-//             row.push_back(mn,{id1,id2});
 //         }
         
-//         for(i=0;i<n;i++)
-//         {
-//             int mx=INT_MIN,id2=i,id1=0;
-//             for(j=0;j<m;j++)
-//             {
-//                 if(mx<mat[i][j])
-//                 {
-//                     id1=j;
-//                 }
-//                 mx=max(mat[j][i],mx);
-//             }
-//             col.push_back(mx,{id1,id2});
-//         }
+        for(i=0;i<m;i++)
+        {
+            int mn=INT_MAX,id1=i,id2=0;
+            for(j=0;j<n;j++)
+            {
+                mn=min(mat[i][j],mn);
+            }
+            row.push_back(mn);
+        }
         
-        // for(i=0;i<)
+        for(i=0;i<n;i++)
+        {
+            int mx=INT_MIN,id2=i,id1=0;
+            for(j=0;j<m;j++)
+            {
+                mx=max(mat[j][i],mx);
+            }
+            col.push_back(mx);
+        }
+        
+        sort(col.begin(),col.end());
+        
+        for(i=0;i<row.size();i++)
+        {
+            if(binary_search(col.begin(),col.end(),row[i])) ans.push_back(row[i]);
+        }
+        
         return ans;
         
     }
