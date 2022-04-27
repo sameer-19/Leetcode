6565a9e686e6139db660ed334402039503122e6f
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-28 00:37:23 +0530

    Time: 7 ms (87.94%), Space: 10.3 MB (36.91%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,14 +5,19 @@ public:
         
         int i,j,n=nums.size();
         
+        int a[102]={0};
+        
+        for(auto x: nums) a[x]++;
+        
+        for(i=1;i<102;i++)
+        {
+            a[i]+=a[i-1];
+        }
+        
         for(i=0;i<n;i++)
         {
-            int cnt=0;
-            for(j=0;j<n;j++)
-            {
-                if(i!=j and nums[j]<nums[i]) cnt++;
-            }
-            ans.push_back(cnt);
+            if(nums[i]==0) ans.push_back(0);
+            else ans.push_back(a[nums[i]-1]);
         }
         
         return ans;
