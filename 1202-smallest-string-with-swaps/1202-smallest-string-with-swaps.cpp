d6a3fc2b3ca9310113c76661a83c72177ab0bc23
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-04-27 23:19:44 +0530

    Time: 253 ms (50.33%), Space: 67.7 MB (25.37%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void dfs(int idx,int n,bool visited[],vector<int> adj[],vector<int> &res)
+    {
+        visited[idx]=true;
+        res.push_back(idx);
+        for(auto x: adj[idx])
+        {
+            if(!visited[x])
+            {
+                dfs(x,n,visited,adj,res);
+            }
+        }
+    }
+    
+    
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        // find all the connected components characters and take all characters and sort them in increasing order
+        int i,j,n=s.length();
+        
+        vector<int> adj[n+1];
+        
+        for(i=0;i<pairs.size();i++){
+            adj[pairs[i][0]].push_back(pairs[i][1]);
+            adj[pairs[i][1]].push_back(pairs[i][0]);
+        }
+        
+        bool visited[100005]={false};
+        
+        for(i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                vector<int> v;
+                dfs(i,n,visited,adj,v);
+                vector<char> ct;
+                for(auto x: v) ct.push_back(s[x]);
+                sort(ct.begin(),ct.end());
+                sort(v.begin(),v.end());
+                int j=0;
+                while(j<v.size())
+                {
+                    s[v[j]]=ct[j];
+                    j++;
+                }
+            }
+        }
+        return s;
+        
+    }
+};
