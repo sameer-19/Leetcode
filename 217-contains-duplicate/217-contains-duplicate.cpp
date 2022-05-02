40e3555090fc389e6fc05b8eec1b27995df05918
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-05-02 10:12:48 +0530

    Time: 172 ms (13.95%), Space: 52.2 MB (26.64%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, bool> myMap;
+        // unordered_map<int, bool> myMap;
+        
+        for (auto& num: nums) {
+            if (myMap.find(num) != myMap.end())
+                return true;
+            else
+                myMap[num] = true;
+        }
+        return false;
+    }
+};
