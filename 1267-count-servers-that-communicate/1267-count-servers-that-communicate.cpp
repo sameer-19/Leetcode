a773dd851b5ef0c311c9e1ef90ebbc75aae18780
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-05-03 16:22:43 +0530

    Time: 72 ms (62.60%), Space: 22.2 MB (91.60%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    int countServers(vector<vector<int>>& a) {
-        int ans=0;
-        int m=a.size(),n=a[0].size();
-        
-        int i,j,k;
-        vector<vector<int>> visited(m,vector<int> (n,0));
-        
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(a[i][j]==1)
-                {
-                    for(k=0;k<m;k++)
-                    {
-                        if(i!=k)
-                        {
-                            if(visited[k][j]==0 and a[k][j]==1) 
-                            {
-                                ans++;
-                                visited[k][j]=1;
-                            }
-                        }
+    int countServers(vector<vector<int>>& grid) {
+        int cnt=0;
+        for(int i=0;i<grid.size();i++){
+            int x=-1,y=-1;
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]==1){
+                    if(x==-1 && y==-1){
+                        x=i; y=j;
                     }
-                    
-                    for(k=0;k<n;k++)
-                    {
-                        if(k!=j)
-                        {
-                            if(visited[i][k]==0 and a[i][k]==1)
-                            {
-                                ans++;
-                                visited[i][k]=1;
-                            }
-                        }
+                    else{
+                        grid[x][y]=2;
+                        grid[i][j]=2;
                     }
-                    
                 }
             }
         }
         
-        return ans;
+        
+            
+        for(int j=0;j<grid[0].size();j++){
+           int x=-1,y=-1;
+            for(int i=0;i<grid.size();i++){
+                if(grid[i][j]!=0){
+                    if(x==-1 && y==-1){
+                        x=i; y=j;
+                    }
+                    else{
+                        grid[x][y]=2;
+                        grid[i][j]=2;
+                    }
+                }
+            }
+        }
+                               
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[i].size();j++)
+                               if(grid[i][j]==2)
+                                    cnt++;
+        
+        return cnt;
     }
 };
