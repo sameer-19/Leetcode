bd1fb783742eb593b617caca6ebf6be69cff56fa
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-05-06 07:27:10 +0530

    Time: 9 ms (7.05%), Space: 6.3 MB (25.73%) - LeetHub

--- 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
@@ -17,22 +17,25 @@ public:
     int minimumSwap(string s1, string s2) {
         if(s1==s2) return 0;
         
-        int ans=0,n=s1.size(),i,cnt=0;
+        int ans=0,n=s1.size(),i;
         
         if(count1(s1,s2,'x')%2!=0 and count1(s1,s2,'y')%2!=0) return -1;
         
-        string s;
+        int x=0,y=0;
         
         for(i=0;i<n;i++)
         {
             if(s1[i]!=s2[i])
             {
-                s.push_back(s1[i]);
+                if(s1[i]=='x') x++; // means x-y pair
+                else y++; // means y-x pair
             }
         }
         
-        int x=count(s.begin(),s.end(),'x');
-        int y=count(s.begin(),s.end(),'y');
+        // x x or y y pair takes 1 swap
+        // y y    x x 
+        // x y or y x pair takes 2 swaps
+        // y x    x y
         
         ans+=(x/2)+x%2;
         ans+=(y/2)+y%2;
