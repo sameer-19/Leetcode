fb4dc8483269c12589c51cab4a63cb2b06d8689a
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-05-08 00:23:04 +0530

    Time: 10 ms (17.96%), Space: 8.5 MB (57.57%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    bool match(string& w1, string& w2) 
+    {
+        unordered_map<char, char> w1_to_w2, w2_to_w1;
+        for(int i = 0; i < size(w1); i++) 
+        {
+            if( (w1_to_w2.count(w1[i]) && w1_to_w2[w1[i]] != w2[i]) || 
+                (w2_to_w1.count(w2[i]) && w2_to_w1[w2[i]] != w1[i]) ) 
+                return false; 
+            else
+                w1_to_w2[w1[i]] = w2[i],
+                w2_to_w1[w2[i]] = w1[i];
+        }    
+        return true;
+    }
+    
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pat) {
+        vector<string> ans;
+        
+        for(auto x: words)
+        {
+            if(match(x,pat)) ans.push_back(x);
+        }
+        
+        return ans;
+    }
+};
