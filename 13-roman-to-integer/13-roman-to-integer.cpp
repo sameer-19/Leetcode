06f783f0948d040fa997192c28bfe9f2847b00ef
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-05-23 17:53:48 +0530

    Time: 23 ms (29.66%), Space: 8 MB (36.00%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         
         mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; mp['C']=100; mp['D']=500;
         mp['M']=1000;
