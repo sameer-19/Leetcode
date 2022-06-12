979dac8a6552bf7291e7c7b04dbc31f88c85850b
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-06-12 09:24:25 +0530

    Time: 405 ms (69.75%), Space: 99.6 MB (82.38%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,9 +4,11 @@ public:
         int n=nums.size(),i;
         int ans=0;
     
+        unordered_map<int,int> mp;
+        
         int left=0,sum=0;
         i=0;
-        map<int,int> mp;
+        
         
         while(i<n)
         {
