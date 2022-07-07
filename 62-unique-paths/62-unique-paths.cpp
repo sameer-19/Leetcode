124acae75c30e8fc84d5bbbe1e3d20ad65a220d7
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-07 17:27:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.69%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int up=0,left=0;
+        up=solve(i-1,j,dp);
+        left=solve(i,j-1,dp);
+        
+        return dp[i][j] = left + up;
+    }
+    
+    
     int uniquePaths(int m, int n) {
-        int a[105][105];
-        int i,j,ans=0;
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(i==0 || j==0) a[i][j]=1;
-            }
-        }
-        for(i=1;i<m;i++)
-        {
-            for(j=1;j<n;j++)
-            {
-                a[i][j]=a[i-1][j]+a[i][j-1];
-            }
-        }
-        ans=a[m-1][n-1];
-        return ans;
+        
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        
+        return solve(m-1,n-1,dp);
+        
+        // int a[105][105];
+        // int i,j,ans=0;
+        // for(i=0;i<m;i++)
+        // {
+        //     for(j=0;j<n;j++)
+        //     {
+        //         if(i==0 || j==0) a[i][j]=1;
+        //     }
+        // }
+        // for(i=1;i<m;i++)
+        // {
+        //     for(j=1;j<n;j++)
+        //     {
+        //         a[i][j]=a[i-1][j]+a[i][j-1];
+        //     }
+        // }
+        // ans=a[m-1][n-1];
+        // return ans;
     }
 };
