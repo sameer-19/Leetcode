7cf69f2a36d6a2e022f6b677e5221a7eab53780e
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-10 10:47:59 +0530

    Time: 6 ms (83.53%), Space: 13.7 MB (79.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        // bottom up approach
-        int dp[cost.size()+1],i,n=cost.size();
+        int n=cost.size();
+        
+        int dp[n+1];
+        cost.push_back(0);
+        
         dp[0]=cost[0];
         dp[1]=cost[1];
-        for(i=2;i<=n;i++)
-        {
-            dp[i]=min(dp[i-1],dp[i-2])+(i==n ? 0 : cost[i]);
-        }
+        
+        for(int i=2;i<=n;i++) dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+        
         return dp[n];
-        // // Top down Approach
-        // int index=cost.size();
-        // int memo[1001]={0};
-        // int z=solve(cost,index,memo);
-        // return z;
     }
 };
