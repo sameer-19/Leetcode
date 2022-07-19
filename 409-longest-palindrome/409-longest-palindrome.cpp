02b62b86a2c9b2449fe5a3735f1d901ca7599960
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-19 16:30:38 +0530

    Time: 11 ms (10.59%), Space: 6.7 MB (14.00%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int a[26]={0},b[26]={0};
+        map<char,int> mp;
         int i,n=s.length();
         
         for(i=0;i<n;i++)
         {
-            if(s[i]-'a'>=0 and s[i]-'a'<=25)
-            a[s[i]-'a']++;
-            else b[s[i]-'A']++;
-        }
-        int ans=0;
-        
-        for(i=0;i<26;i++)
-        {
-            if(a[i]%2==0) ans+=a[i],a[i]=0;
-            else ans+=a[i]-1,a[i]=1;
-        }
-        for(i=0;i<26;i++)
-        {
-            if(b[i]%2==0) ans+=b[i],b[i]=0;
-            else ans+=b[i]-1,b[i]=1;
+            mp[s[i]]++;
         }
         
-        for(i=0;i<26;i++)
+        int ans=0,f=0;
+        
+        for(auto x: mp)
         {
-            if(a[i]==1) 
+            if(x.second%2!=0) 
             {
-                ans++;
-                break;
+                ans+=(x.second-1);
+                f=1;
+            }
+            else
+            {
+                ans+=x.second;
             }
         }
         
-        if(ans%2==0)
-        {
-            for(i=0;i<26;i++)
-            {
-                if(b[i]==1) 
-                {
-                    ans++;
-                    break;
-                }
-            }
-        }
+        if(f) ans++;
         
         return ans;
     }
