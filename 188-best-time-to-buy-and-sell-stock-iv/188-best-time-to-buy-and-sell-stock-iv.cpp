09af5f12adecf450e8ac65c8312e6210dad00838
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-21 17:08:18 +0530

    Time: 15 ms (61.61%), Space: 13 MB (31.17%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    // Memoization
     int solve(int idx,int buy,int cap,vector<int>& prices,
                                 vector<vector<vector<int>>> &dp)
     {
@@ -27,8 +29,30 @@ public:
         int n=prices.size();
         
         vector<vector<vector<int>>> dp(n+1, 
-                    vector<vector<int>> (2, vector<int> (k+1,-1)));   
+                    vector<vector<int>> (2, vector<int> (k+1, 0)));
         
-        return solve(0,1,k,prices,dp);
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                    int profit=0;
+        
+                    if(buy)
+                    {
+            profit = max(-prices[idx]+dp[idx+1][0][cap],0 + dp[idx+1][1][cap]);
+                    }
+                    else
+                    { 
+            profit=max(prices[idx]+dp[idx+1][1][cap-1], 0 + dp[idx+1][0][cap]); 
+                    }
+                    
+                    dp[idx][buy][cap]=profit;
+                }
+            }
+        }
+        
+        return dp[0][1][k];
     }
 };
