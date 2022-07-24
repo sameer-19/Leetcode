1e31f0c7634dcef3ae0ebe292ba477c2df28c8ab
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-24 15:06:11 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (39.88%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,13 +4,13 @@ public:
     // Memoization
     int solve(int i,int n,vector<int> &dp)
     {
-        if(i>n) return 0;
-        if(i==n) return 1;
+        if(i==0) return 1;
+        if(i==1) return 2;
         if(dp[i]!=-1) return dp[i];
         
         int ans=0;
-        ans += solve(i+1,n,dp);
-        ans+=solve(i+2,n,dp);
+        ans += solve(i-1,n,dp);
+        ans += solve(i-2,n,dp);
         
         return dp[i] = ans;
     }
@@ -18,14 +18,17 @@ public:
     int climbStairs(int n) {
         if(n==1 || n==2) return n;
         int prev = 1, cur=2;
+        vector<int> dp(n+1,-1);
         
-        for(int i=3;i<=n;i++)
-        {
-            int tmp = prev+cur;
-            prev=cur;
-            cur=tmp;
-        }
+        return solve(n-1,n,dp);
         
-        return cur;
+//         for(int i=3;i<=n;i++)
+//         {
+//             int tmp = prev+cur;
+//             prev=cur;
+//             cur=tmp;
+//         }
+        
+//         return cur;
     }
 };
