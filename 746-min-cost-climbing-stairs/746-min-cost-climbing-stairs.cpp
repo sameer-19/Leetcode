2356b35455574b2fe24d4141f2bfed98d6b25293
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-26 18:14:10 +0530

    Time: 18 ms (15.00%), Space: 14 MB (9.41%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    // Memoization
+    
     int solve(int idx,vector<int>&cost,vector<int> &dp)
     {
         if(idx==0) return cost[0];
         if(idx==1) return cost[1];
-        
         if(dp[idx]!=-1) return dp[idx];
         
-        int ans=0;
-        
-ans+=min(solve(idx-1,cost,dp),solve(idx-2,cost,dp)) + (idx==cost.size()?0:cost[idx]);
-     
-        return dp[idx] = ans;
+return dp[idx] = min(solve(idx-1,cost,dp),solve(idx-2,cost,dp)) +
+                            (idx==cost.size() ? 0: cost[idx]);
     }
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        if(n==1) return cost[0];
-        if(n==2) return min(cost[0],cost[1]);
+        vector<int> dp(n+1,-1);
         
-        int prev,cur;
-        
-        prev = cost[0];
-        cur = cost[1];
-        
-        for(int i=2;i<=n;i++)
-        {
-            int tmp = min(prev,cur) + (i==n ? 0 : cost[i]);
-            prev=cur;
-            cur=tmp;
-        }
-        
-        return cur;
+        return solve(n,cost,dp);
     }
 };
