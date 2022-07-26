df60e146e21118a03518fdf51a2bea739f73f405
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-26 18:52:07 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -18,8 +18,21 @@ public:
     
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        vector<vector<int>> dp(m,vector<int> (n,0));
         
-        return solve(m-1,n-1,m,n,dp);
+        int i,j;
+        
+        for(i=0;i<m;i++) dp[i][0]=1;
+        for(j=0;j<n;j++) dp[0][j]=1;
+        
+        for(i=1;i<m;i++)
+        {
+            for(j=1;j<n;j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
