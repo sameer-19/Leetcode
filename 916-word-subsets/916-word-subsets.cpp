773029dbf6f59cf6cc9b589869f20f53119d2c53
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-30 15:16:01 +0530

    Time: 192 ms (89.44%), Space: 57.5 MB (80.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<string> ans;
+    bool check(string s1,vector<int> &v)
+    {
+        int a[26]={0},i,j;
         
-        int freq[26]={0};
-        int i,j;
+        for(i=0;i<s1.length();i++) a[s1[i]-'a']++;
         
-        for(i=0;i<words2.size();i++)
+        for(i=0;i<26;i++)
         {
-            int a[26]={0};
-            for(j=0;j<words2[i].size();j++) a[words2[i][j]-'a']++;
-            for(j=0;j<26;j++) freq[j]=max(freq[j],a[j]);
+            if(v[i]>a[i]) return false;
         }
         
-        for(i=0;i<words1.size();i++)
+        return true;
+    }
+    
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        
+        vector<string> ans;
+        
+        int i,j,m=words1.size();
+        
+        vector<int> arr(26,0);
+        for(i=0;i<words2.size();i++)
         {
-            int a[26]={0},f=0;
-            for(j=0;j<words1[i].size();j++) a[words1[i][j]-'a']++;
+            int b[26]={0};
+            for(j=0;j<words2[i].length();j++) b[words2[i][j]-'a']++;
             
             for(j=0;j<26;j++)
             {
-                if(a[j]<freq[j]) {f=1; break;}
+                arr[j]=max(arr[j],b[j]);
             }
-            if(f==0) ans.push_back(words1[i]);
             
         }
         
+        for(i=0;i<m;i++)
+        {
+            if(check(words1[i],arr))
+            {
+                ans.push_back(words1[i]);
+            }
+        }
+        
         return ans;
+        
     }
 };
