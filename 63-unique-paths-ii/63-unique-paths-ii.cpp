ffd2e7b217a3f5f6b2e413ee029aa846f4515120
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-30 20:42:44 +0530

    Time: 3 ms (84.65%), Space: 7.6 MB (91.12%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -23,25 +23,23 @@ public:
         int m=grid.size(),n=grid[0].size(),i,j;
         if(grid[m-1][n-1]==1 || grid[0][0]==1) return 0;
         
-        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+        grid[0][0]=1;
         
-        dp[0][0]=1;
-        
-        for(i=1;i<n;i++) dp[0][i]=(grid[0][i]==0 ? dp[0][i-1] : 0);
-        for(i=1;i<m;i++) dp[i][0]=(grid[i][0]==0 ? dp[i-1][0] : 0);
+        for(i=1;i<n;i++) grid[0][i]=(grid[0][i]==0 ? grid[0][i-1] : 0);
+        for(i=1;i<m;i++) grid[i][0]=(grid[i][0]==0 ? grid[i-1][0] : 0);
         
         for(i=1;i<m;i++)
         {
             for(j=1;j<n;j++)
             {
-                if(grid[i][j]==1) dp[i][j]=0;
+                if(grid[i][j]==1) grid[i][j]=0;
                 else
                 {
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    grid[i][j]=grid[i-1][j]+grid[i][j-1];
                 }
             }
         }
         
-        return dp[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
