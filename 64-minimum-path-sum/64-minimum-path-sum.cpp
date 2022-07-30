024f89c0ce4095b88e31b08b34397defed56f0a6
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-07-31 00:15:02 +0530

    Time: 11 ms (80.99%), Space: 10.1 MB (29.03%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    int solve(int i,int j,int m,int n,vector<vector<int>>& grid,
+                                                        vector<vector<int>> &dp)
+    {
+        if(i<0 || i>=m || j<0 || j>=n) return 1e9;
+        if(i==0 and j==0) return grid[i][j];
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int ans;
+        
+        ans= min(solve(i-1,j,m,n,grid,dp),solve(i,j-1,m,n,grid,dp)) + grid[i][j];
+        
+        return dp[i][j]= ans;
+    }
+    
+    
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int m=grid.size(),n=grid[0].size();
         
-        int i,j;
+        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
         
-        vector<int> prev(n,0);
-        vector<int> cur(n,0);
-        
-        prev[0]=grid[0][0];
-        
-        for(i=1;i<n;i++) prev[i]=prev[i-1]+grid[0][i];
-        
-        for(i=1;i<m;i++)
-        {
-            cur[0]=prev[0]+grid[i][0];
-            
-            for(j=1;j<n;j++) cur[j]=min(cur[j-1],prev[j])+grid[i][j];
-            
-            prev=cur;
-        }
-        
-        return prev[n-1];
+        return solve(m-1,n-1,m,n,grid,dp);
     }
 };
