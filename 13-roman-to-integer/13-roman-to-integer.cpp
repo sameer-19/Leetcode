9192f41acc043d4510e2fdebc86c154fe78010ff
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-15 13:56:04 +0530

    Time: 18 ms (53.10%), Space: 8.2 MB (21.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> mp;
+        int n=s.length(),i,j;
         
-        mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; mp['C']=100; mp['D']=500;
-        mp['M']=1000;
+        map<char,int> mp;
+        mp['I']=1;mp['V']=5;mp['X']=10;mp['L']=50;mp['C']=100;mp['D']=500;mp['M']=1000;
         
-        int ans=0,n=s.length(),i;
+        int ans=0;
+        ans=mp[s[n-1]];
         
-        if(n==1) return mp[s[0]];
-        
-        for(i=0;i<n-1;++i)
+        for(i=n-2;i>=0;i--)
         {
-            if(mp[s[i]]<mp[s[i+1]]) 
+            if(s[i]=='I')
             {
-                ans+=mp[s[i+1]]-mp[s[i]];
-                ++i;
+                if(s[i+1]=='V' || s[i+1]=='X') 
+                {
+                    ans-=1;
+                }
+                else ans++;
             }
-            else
+            else if(s[i]=='X')
             {
-                ans+=mp[s[i]];
+                if(s[i+1]=='L' || s[i+1]=='C')
+                {
+                    ans-=10;
+                }
+                else ans+=10;
             }
+            else if(s[i]=='C')
+            {
+                if(s[i+1]=='D' || s[i+1]=='M') ans-=100;
+                else ans+=100;
+            }
+            else ans+=mp[s[i]];
         }
-        
-        if(i!=n) ans+=mp[s[n-1]];
-        
         
         return ans;
     }
