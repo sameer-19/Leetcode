511c56def3158d57b77ecf84cf419704a8697daa
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-17 20:10:46 +0530

    Time: 201 ms (68.94%), Space: 91.5 MB (72.19%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -13,15 +13,22 @@ public:
     }
     
     int arrayNesting(vector<int>& nums) {
-        int ans=0,cnt=0,n=nums.size();
+        int ans=0,cnt=0,i,j,n=nums.size();
         
-        for(int i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
             if(nums[i]!=-1)
             {
-                dfs(i,nums,cnt);
-                ans=max(ans,cnt);
                 cnt=0;
+                j=i;
+                while(nums[j]>=0)
+                {
+                    cnt++;
+                    int m=nums[j];
+                    nums[j]=-1;
+                    j=m;
+                }
+                ans=max(ans,cnt);
             }
         }
         
