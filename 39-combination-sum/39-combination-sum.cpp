b9c6705c744baeb5951992609d474fd0065615f6
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-24 11:41:19 +0530

    Time: 123 ms (22.53%), Space: 44.3 MB (22.35%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+void solve(int idx,int target,int sum,vector<int>&nums,vector<int>res,vector<vector<int>> &ans)
+    {
+        if(idx<0)
+        {
+            if(sum==target) ans.push_back(res);
+            
+            return;
+        }
+        
+        if(sum>target) return;
+        
+        solve(idx-1,target,sum,nums,res,ans);
+        
+        if(nums[idx]+sum<=target) 
+        {
+            sum+=nums[idx];    
+            res.push_back(nums[idx]);
+            solve(idx,target,sum,nums,res,ans);
+        }
+    
+        return;
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        
+        vector<int> res;
+        
+        sort(nums.begin(),nums.end());
+        
+        int n=nums.size(),sum=0;
+        
+        solve(n-1,target,sum,nums,res,ans);
+        
+        return ans;
+    }
+};
