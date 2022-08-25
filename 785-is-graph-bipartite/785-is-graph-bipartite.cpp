55c3774567d11637745920214cfcc3ceb866ea0d
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-25 19:20:50 +0530

    Time: 109 ms (5.04%), Space: 21.4 MB (5.02%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,77 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,0);    
-        for(int i=0;i<n;i++)                 // traverse each component
+    
+    bool bipartiteDFS(int node,vector<int> adj[],vector<int> &color)
+    {
+        if(color[node]==-1) color[node]=1;
+        
+        for(auto child: adj[node])
         {
-            if(color[i]) continue;          // if already coloured then continue
-            queue<int> q;
-            q.push(i);
-            color[i]=1;
-            while(!q.empty())
+            if(color[child]==-1)
             {
-                int parent=q.front();
-                q.pop();
-                for(int child:graph[parent])
+                color[child]=1-color[node];
+                if(!bipartiteDFS(child,adj,color)) return false;
+            }
+            else if(color[child]==color[node]) return false;
+        }
+        
+        return true;
+    }
+    
+    bool checkBipartite(vector<int> adj[],int n)
+    {
+        int i;
+        
+        vector<int> color(n,-1);
+        
+        for(i=0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(!bipartiteDFS(i,adj,color)) return false;
+            }
+        }
+        
+        // for(i=0;i<n;i++)
+        // {
+        //     if(color[i]==-1)
+        //     {
+        //         if(!bipartiteBFS(i,adj,color)) return false;
+        //     }
+        // }
+        
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        // graph is bipartite if it does not contain odd length cycle
+        int n=graph.size(),i,j;
+        
+        vector<int> adj[n];
+        
+        set<pair<int,int>> s;
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<graph[i].size();j++)
+            {
+                if(i<graph[i][j])
                 {
-                    if(color[child]==0)
-                    {
-                        color[child]=-color[parent];
-                        q.push(child);
-                    }
-                    else
-                    {
-                        if(color[parent]==color[child]) return false;    
-                        // if parent and child have same colour then return false;
-                    }
+                    s.insert({i,graph[i][j]});
+                }
+                else
+                {
+                    s.insert({graph[i][j],i});
                 }
             }
-
         }
-        return true;
+        
+        for(auto x: s)
+        {
+            adj[x.first].push_back(x.second);
+            adj[x.second].push_back(x.first);
+        }
+        
+        return checkBipartite(adj,n);
     }
 };
