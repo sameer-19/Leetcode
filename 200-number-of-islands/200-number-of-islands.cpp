989b2ee96eb4c4decd2baffcc416481f9f7bb54d
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-29 09:13:14 +0530

    Time: 53 ms (56.95%), Space: 18.2 MB (14.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
-    void dfs(int i,int j,int m,int n,vector<vector<char>> &grid,
-                                                        vector<vector<bool>> &visited)
-    {
-        if(i<0 || i>=m || j<0 || j>=n) return;
-        if(grid[i][j]=='0') return;
-        if(visited[i][j]) return;
-        
-        visited[i][j]=true;
-        
-        int dx[4]={0,0,1,-1},dy[4]={1,-1,0,0};
-        
-        for(int k=0;k<4;k++)
-        {
-            int x=i+dx[k],y=j+dy[k];
-            
-            dfs(x,y,m,n,grid,visited);
-        }
-        
-        return;
-    }
-    
     int numIslands(vector<vector<char>>& grid) {
-        int ans=0,i,j;
-        
-        int m=grid.size(),n=grid[0].size();
-        
-        vector<vector<bool>> visited(m,vector<bool> (n,false));
-        
-        for(i=0;i<m;i++)
+        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
+        for (int i = 0; i < m; i++) 
         {
-            for(j=0;j<n;j++)
+            for (int j = 0; j < n; j++) 
             {
-                if(grid[i][j]=='1')
+                if (grid[i][j] == '1') 
                 {
-                    if(!visited[i][j])
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> todo;
+                    todo.push({i, j});
+                    while (!todo.empty()) 
                     {
-                        ans++;
-                        dfs(i,j,m,n,grid,visited);
+                        pair<int, int> p = todo.front();
+                        todo.pop();
+                        for (int k = 0; k < 4; k++) 
+                        {
+                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') 
+                            {
+                                grid[r][c] = '0';
+                                todo.push({r, c});
+                            }
+                        }
                     }
                 }
             }
         }
-        
-        return ans;
+        return islands;
     }
 };
