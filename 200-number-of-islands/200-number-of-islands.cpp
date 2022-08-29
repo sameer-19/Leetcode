3981eec17ed8fb7df50a32feecfd7238b089d3ec
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-08-29 09:01:37 +0530

    Time: 49 ms (64.88%), Space: 12.6 MB (40.12%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    void dfs(int i,int j,int m,int n,
-                    vector<vector<char>>&grid,vector<vector<bool>>& visited)
+    void dfs(int i,int j,int m,int n,vector<vector<char>> &grid,
+                                                        vector<vector<bool>> &visited)
     {
         if(i<0 || i>=m || j<0 || j>=n) return;
         if(grid[i][j]=='0') return;
-        if(visited[i][j]==true) return;
+        if(visited[i][j]) return;
         
-        visited[i][j] = true;
+        visited[i][j]=true;
         
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
+        int dx[4]={0,0,1,-1},dy[4]={1,-1,0,0};
         
         for(int k=0;k<4;k++)
         {
-            dfs(i+dx[k],j+dy[k],m,n,grid,visited);
+            int x=i+dx[k],y=j+dy[k];
+            
+            dfs(x,y,m,n,grid,visited);
         }
         
         return;
     }
-        
+    
     int numIslands(vector<vector<char>>& grid) {
-        int i,j,ans=0;
+        int ans=0,i,j;
         
         int m=grid.size(),n=grid[0].size();
         
-        vector<vector<bool>> visited(m+1, vector<bool> (n+1,false));
+        vector<vector<bool>> visited(m,vector<bool> (n,false));
         
         for(i=0;i<m;i++)
         {
