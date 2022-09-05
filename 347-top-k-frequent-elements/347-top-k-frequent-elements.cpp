87fdd1a94d5928762205f85a8966992ae510d0a9
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-09-05 22:49:55 +0530

    Time: 36 ms (17.21%), Space: 13.5 MB (97.54%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,43 @@
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> a;
-        int i;
-        for(i=0;i<nums.size();i++) a[nums[i]]++; // Time Complexity - O(N)
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        
-        for(auto x: a) // Time Complexity - O(N)
+class Solution
+{
+    public:
+        vector<int> topKFrequent(vector<int> &nums, int k)
         {
-            pq.push({x.second,x.first});
-            if(pq.size()>k) pq.pop();
+            unordered_map<int, int> mp;
+
+            int i, j, n = nums.size();
+
+            for (i = 0; i < n; i++)
+            {
+                mp[nums[i]]++;
+            }
+
+            priority_queue<pair<int, int>,vector<pair<int,int>>, greater<pair<int, int>>> pq;
+
+            for (auto x: mp)
+            {
+                if (pq.size() < k)
+                {
+                    pq.push({ x.second, x.first });
+                }
+                else
+                {
+                    if (pq.top().first < x.second)
+                    {
+                        pq.pop();
+                        pq.push({ x.second, x.first });
+                    }
+                }
+            }
+
+            vector<int> ans;
+
+            while (!pq.empty())
+            {
+                ans.push_back(pq.top().second);
+                pq.pop();
+            }
+
+            return ans;
         }
-        // Time complexity of above - O(N*log(K))
-        vector<int> v;
-        while(!pq.empty()) // Time complexity - O(K)
-        {
-            v.push_back(pq.top().second);
-            pq.pop();
-        }
-        return v;
-    }
 };
