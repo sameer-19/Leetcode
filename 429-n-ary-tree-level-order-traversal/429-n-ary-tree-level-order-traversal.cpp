0a23b8474bb9aed108332f2f5d0326bd6f489ce1
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-09-05 08:32:12 +0530

    Time: 53 ms (10.97%), Space: 11.9 MB (34.37%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -18,39 +18,50 @@ public:
 };
 */
 
-class Solution {
-public:
-    vector<vector<int>> levelOrder(Node* root) {
-        vector<vector<int>> ans;
-        
-        if(!root) return ans;
-        
-        queue<Node*> q;
-        
-        q.push(root);
-        
-        while(!q.empty())
+class Solution
+{
+    public:
+        vector<vector < int>> solve(Node *root)
         {
-            int size = q.size();
-            vector<int> result;
-            for(int i=0;i<size;i++)
+            vector<vector < int>> ans;
+
+            queue<Node*> q;
+
+            q.push(root);
+
+            while (!q.empty())
             {
-                Node* node = q.front();
-                q.pop();
-                result.push_back(node->val);
-                
-                for(auto child: node->children)
+                int size = q.size();
+
+                vector<int> result;
+
+                for (int i = 0; i < size; i++)
                 {
-                    if(child)
+                    Node *node = q.front();
+                    q.pop();
+                    result.push_back(node->val);
+
+                    for (auto child: node->children)
                     {
-                        q.push(child);   
+                        if (child)
+                        {
+                            q.push(child);
+                        }
                     }
                 }
+
+                ans.push_back(result);
             }
-            
-            ans.push_back(result);
+
+            return ans;
         }
-        
-        return ans;
+
+    vector<vector < int>> levelOrder(Node *root)
+    {
+        vector<vector < int>> ans;
+
+        if (!root) return ans;
+
+        return solve(root);
     }
 };
