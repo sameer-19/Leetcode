c7f3cb887316e3f0b87c3f8e68d63ea5653cb195
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-09-08 17:39:41 +0530

    Time: 34 ms (23.03%), Space: 19.8 MB (61.39%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,72 +1,58 @@
 class Solution {
 public:
-    
-    int bruteForce(vector<int>& h)
+    int bruteForce(vector<int> &height)
     {
-        int ans=0,i,j,n=h.size();
+        int i,j,n=height.size();
+        
+        int ans=0;
         
         for(i=0;i<n;i++)
         {
-            int lmax=h[i],rmax=h[i];
-            for(j=i-1;j>=0;j--) lmax=max(lmax,h[j]);
-            for(j=i+1;j<n;j++) rmax=max(rmax,h[j]);
-            
+            int lmax=height[i],rmax=height[i];
+            j=i-1;
+            while(j>=0)
+            {
+                lmax=max(lmax,height[j--]);
+            }
+            j=i+1;
+            while(j<n)
+            {
+                rmax=max(rmax,height[j++]);    
+            }
             int mn=min(lmax,rmax);
-            ans+=mn-h[i];
+            ans+=mn-height[i];
         }
         
         return ans;
     }
     
-    int optimalSolution(vector<int> &h)
+    int optimal(vector<int> &height)
     {
-        int i,n=h.size(),ans=0;
+        int n=height.size(),i,j,ans=0;
+        int pre[n],suf[n];
         
-        vector<int> prefMax(n,0),suffMax(n,0);
-        prefMax[0]=h[0];
-        suffMax[n-1]=h[n-1];
-        
-        for(i=1;i<n;i++) prefMax[i]=max(prefMax[i-1],h[i]);
-        for(i=n-2;i>=0;i--) suffMax[i]=max(suffMax[i+1],h[i]);
+        pre[0]=height[0];
+        suf[n-1]=height[n-1];
+        for(i=1;i<n;i++)
+        {
+            pre[i]=max(pre[i-1],height[i]);
+        }
+        for(i=n-2;i>=0;i--)
+        {
+            suf[i]=max(suf[i+1],height[i]);
+        }
         
         for(i=0;i<n;i++)
         {
-            ans+=(min(prefMax[i],suffMax[i]))-h[i];
-        }
-        
-        return ans;
-    }
-    
-    int optimal(vector<int> &h)
-    {
-        int n=h.size(),left,right;
-        left=0;right=n-1;
-        
-        int ans=0,lmax=0,rmax=0;
-        
-        while(left<right)
-        {
-            if(h[left]<=h[right])
-            {
-                lmax=max(lmax,h[left]);
-                ans+=lmax-h[left];
-                left++;
-            }
-            else
-            {
-                rmax=max(rmax,h[right]);
-                ans+=rmax-h[right];
-                right--;
-            }
+            ans+=min(pre[i],suf[i])-height[i];
         }
         
         return ans;
     }
     
     int trap(vector<int>& height) {
-        // return bruteForce(height);
-        // return optimalSolution(height);
-        return optimal(height);
         
+        return optimal(height);
+        // return bruteForce(height);
     }
 };
