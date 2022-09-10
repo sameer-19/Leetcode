5fc5f1493d19c73e4238463a7d0a8845fe35f1a2
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-09-10 12:23:45 +0530

    Time: 17 ms (54.50%), Space: 20.1 MB (99.63%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,30 +1,39 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int mx=0;
-    int solve(TreeNode* root)
-    {
-        if(!root) return 0;
-        int lh=solve(root->left);
-        int rh=solve(root->right);
-        mx=max(mx,lh+rh);
-        return max(lh,rh)+1;
-    }
+class Solution
+{
+    public:
+        int solve(TreeNode* root,int &ans)
+        {
+            if (!root) return 0;
+            if (!root->left and!root->right) return 1;
+
+            int left = solve(root->left,ans);
+            int right = solve(root->right,ans);
+            
+            ans=max(ans,left+right);
+            
+            return max(left,right)+1;
+        }
     
-    
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
-        int ans=solve(root);
-        return mx;
-    }
+        int diameterOfBinaryTree(TreeNode *root)
+        {
+            if (!root) return 0;
+            if (!root->left and!root->right) return 0;
+            
+            int ans=0;
+            
+            solve(root,ans);
+            
+            return ans;
+        }
 };
