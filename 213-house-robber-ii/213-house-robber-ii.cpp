59a3890ae033257f64f0e1df342b241c1e34653d
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-09-14 11:48:54 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (6.93%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    
-    int solve(int idx,vector<int>& nums,int flag,vector<vector<int>>& dp)
+    int solve(int idx,int flag,vector<int> &nums,vector<vector<int>> &dp)
     {
         if(idx<0) return 0;
-        if(idx==0) return (flag==1) ? 0 : nums[idx];
+        
+        if(idx==0)
+        {
+            if(flag==0) return 0;
+        }
         
         if(dp[idx][flag]!=-1) return dp[idx][flag];
         
-    int ans= max(solve(idx-2,nums,flag,dp)+nums[idx],solve(idx-1,nums,flag,dp));
+        int take = nums[idx]+solve(idx-2,flag,nums,dp);
+        int notTake = solve(idx-1,flag,nums,dp);
         
-        return dp[idx][flag] = ans;
+        return dp[idx][flag] = max(take,notTake);
     }
     
     int rob(vector<int>& nums) {
-        int flag=1,n=nums.size();
+        int ans=0;
+        int n=nums.size();
         if(n==1) return nums[0];
         
-        vector<vector<int>> dp(n+1, vector<int> (3,-1));
+        vector<vector<int>> dp(n,vector<int> (2,-1));
         
-        return max(solve(n-1,nums,flag,dp),solve(n-2,nums,0,dp));
+        ans=max(solve(n-1,0,nums,dp),solve(n-2,1,nums,dp));
+        
+        return ans;
     }
 };
