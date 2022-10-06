d78dedb1b04487e8d67e0a024a997b4887e1ac07
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-10-06 23:54:35 +0530

    Time: 38 ms (38.48%), Space: 25 MB (37.01%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -1,69 +1,64 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if(depth==1)
+class Solution
+{
+    public:
+        TreeNode* addOneRow(TreeNode *root, int val, int depth)
         {
-            TreeNode* node = new TreeNode(val);
-            node->left = root;
-            
-            return node;
-        }
-        else
-        {
-            queue<TreeNode*> q;
-            
-            q.push(root);
-            
-            while(!q.empty())
+            if (depth == 1)
             {
-                int sz=q.size();
-                depth--;
-                if(depth==1) break;
-                
-                for(int i=0;i<sz;i++)
-                {
-                    TreeNode* node = q.front();
-                    q.pop();    
-                    
-                    if(node->left) q.push(node->left);
-                    if(node->right) q.push(node->right);
-                }    
-                
+                TreeNode *node = new TreeNode(val);
+                node->left = root;
+
+                return node;
             }
-            
-            while(!q.empty())
+            else
             {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                TreeNode* newNodeLeft = new TreeNode(val);
-                TreeNode* newNodeRight = new TreeNode(val);
-                
-                if(node->left)
+                queue<TreeNode*> q;
+
+                q.push(root);
+
+                while (!q.empty())
                 {
-                    newNodeLeft->left = node->left;
+                    int sz = q.size();
+                    depth--;
+                    if (depth == 1) break;
+
+                    for (int i = 0; i < sz; i++)
+                    {
+                        TreeNode *node = q.front();
+                        q.pop();
+
+                        if (node->left) q.push(node->left);
+                        if (node->right) q.push(node->right);
+                    }
                 }
-                node->left = newNodeLeft;
-                
-                if(node->right)
+
+                while (!q.empty())
                 {
-                    newNodeRight->right = node->right;
+                    TreeNode *node = q.front();
+                    q.pop();
+
+                    TreeNode *newNodeLeft = new TreeNode(val);
+                    TreeNode *newNodeRight = new TreeNode(val);
+
+                    if (node->left) newNodeLeft->left = node->left;
+                    node->left = newNodeLeft;
+
+                    if (node->right) newNodeRight->right = node->right;
+                    node->right = newNodeRight;
                 }
-                node->right = newNodeRight;
+
+                return root;
             }
-            
-            return root;
         }
-    }
 };
