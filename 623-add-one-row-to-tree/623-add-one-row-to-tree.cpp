03d5f7969b338e13f35c69cb58eca6f1dba1c206
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-10-06 23:51:58 +0530

    Time: 27 ms (76.12%), Space: 25.1 MB (16.62%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1)
+        {
+            TreeNode* node = new TreeNode(val);
+            node->left = root;
+            
+            return node;
+        }
+        else
+        {
+            queue<TreeNode*> q;
+            
+            q.push(root);
+            
+            while(!q.empty())
+            {
+                int sz=q.size();
+                depth--;
+                if(depth==1) break;
+                
+                for(int i=0;i<sz;i++)
+                {
+                    TreeNode* node = q.front();
+                    q.pop();    
+                    
+                    if(node->left) q.push(node->left);
+                    if(node->right) q.push(node->right);
+                }    
+                
+            }
+            
+            while(!q.empty())
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                TreeNode* newNodeLeft = new TreeNode(val);
+                TreeNode* newNodeRight = new TreeNode(val);
+                
+                if(node->left)
+                {
+                    newNodeLeft->left = node->left;
+                }
+                node->left = newNodeLeft;
+                
+                if(node->right)
+                {
+                    newNodeRight->right = node->right;
+                }
+                node->right = newNodeRight;
+            }
+            
+            return root;
+        }
+    }
+};
