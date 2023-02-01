30d247e487cd43bb713b9ca0ffb912a9f659b601
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-02-01 20:23:02 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (43.04%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
+    }
+};
