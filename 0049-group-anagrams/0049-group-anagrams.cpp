2d11b235c4ad9240ecd58329e47cab50c2b77e69
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-07-29 17:32:59 +0530

    Time: 43 ms (50.73%), Space: 30.8 MB (5.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,31 @@
-class Solution
-{
-    public:
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
+class Solution {
+public:
+    vector<int> solve(string s)
+    {
+        vector<int> ans(26,0);
+        for(int i=0;i<s.length();i++)
         {
-            unordered_map<string, vector < string>> mp;
-            for (string s: strs)
-            {
-                string t = s;
-                sort(t.begin(), t.end());
-                mp[t].push_back(s);
-            }
-            vector<vector < string>> anagrams;
-            for (auto p: mp)
-            {
-                anagrams.push_back(p.second);
-            }
-            return anagrams;
+            ans[s[i]-'a']++;
         }
+        return ans;
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        
+        map<vector<int>,vector<string>> mp;
+    
+        for(int i=0;i<strs.size();i++)
+        {
+            vector<int> res= solve(strs[i]);
+            mp[res].push_back(strs[i]);
+        }
+        
+        for(auto x: mp) 
+        {
+            result.push_back(x.second);
+        }
+        
+        return result;
+    }
 };
