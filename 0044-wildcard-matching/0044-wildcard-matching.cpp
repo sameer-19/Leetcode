cffb4a3e9ed014edfb13ac86205edcabeef34568
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-08-07 12:33:24 +0530

    Time: 63 ms (60.84%), Space: 9.6 MB (81.16%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -53,7 +53,42 @@ public:
     
     bool isMatch(string s, string p) {
         int m=p.size(),n = s.size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
-        return solve2(m,n,p,s,dp);
+        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
+        
+        // Tabulation
+        dp[0][0]=true;
+        
+        for(int j=1;j<=n;j++)
+        {
+            dp[0][j]=false;
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            bool flag=true;
+            for(int k=1;k<=i;k++)
+            {
+                if(p[k-1]!='*') 
+                {
+                    flag=false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(p[i-1]==s[j-1] || p[i-1]=='?') dp[i][j]= dp[i-1][j-1];
+                else if(p[i-1]=='*')
+                {
+                     dp[i][j]= (dp[i-1][j]|dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
