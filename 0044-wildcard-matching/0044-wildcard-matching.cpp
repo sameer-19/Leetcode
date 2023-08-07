30c78b0db6755a730cb3ac2109133b988f47bf9f
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-08-07 13:01:18 +0530

    Time: 49 ms (81.33%), Space: 9.7 MB (79.00%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -51,11 +51,8 @@ public:
         return dp[i][j]= false;
     }
     
-    bool isMatch(string s, string p) {
-        int m=p.size(),n = s.size();
-        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
-        
-        // Tabulation
+    bool tabulation(int m,int n,string &p,string &s,vector<vector<bool>> &dp)
+    {
         dp[0][0]=true;
         
         for(int j=1;j<=n;j++)
@@ -86,9 +83,59 @@ public:
                 {
                      dp[i][j]= (dp[i-1][j]|dp[i][j-1]);
                 }
+                else dp[i][j]=false;
             }
         }
         
         return dp[m][n];
+    }
+    
+    bool spaceOptimization(int m,int n,string &p,string &s)
+    {
+        vector<bool> prev(n+1,false),cur(n+1,false);
+        prev[0]=true;
+        
+        for(int j=1;j<=n;j++)
+        {
+            prev[j]=false;
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            bool flag=true;
+            for(int k=1;k<=i;k++)
+            {
+                if(p[k-1]!='*') 
+                {
+                    flag=false;
+                    break;
+                }
+            }
+            cur[0] = flag;
+            
+            for(int j=1;j<=n;j++)
+            {
+                if(p[i-1]==s[j-1] || p[i-1]=='?') cur[j]= prev[j-1];
+                else if(p[i-1]=='*')
+                {
+                     cur[j]= prev[j]|cur[j-1];
+                }
+                else cur[j]=false;
+            }
+            prev = cur;
+        }
+        
+        return prev[n];
+    }
+    
+    bool isMatch(string s, string p) {
+        int m=p.size(),n = s.size();
+        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
+        
+        // Tabulation
+        // return tabulation(m,n,p,s,dp);
+        
+        // Space Optimization
+        return spaceOptimization(m,n,p,s);
     }
 };
