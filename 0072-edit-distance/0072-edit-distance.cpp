ff65186a4a5076614aaf0b4bc36475d64052d86a
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-08-07 16:51:09 +0530

    Time: 6 ms (93.05%), Space: 9.4 MB (21.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    int recursion(int i,int j,string &s1,string &s2)
+    {
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        
+        if(s1[i]==s2[j]) return 0 + recursion(i-1,j-1,s1,s2);
+        
+    return 1+ min({recursion(i,j-1,s1,s2),recursion(i-1,j,s1,s2),recursion(i-1,j-1,s1,s2)});
+    }
+    
+    int memo(int i,int j,string &s1,string &s2,vector<vector<int>> &dp)
+    {
+        if(i==0) return j;
+        if(j==0) return i;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s1[i-1]==s2[j-1]) return dp[i][j] = 0 + memo(i-1,j-1,s1,s2,dp);
+        
+        return dp[i][j] = 1+ min({memo(i,j-1,s1,s2,dp),
+                       memo(i-1,j,s1,s2,dp),
+                       memo(i-1,j-1,s1,s2,dp)});
+    }
+    
     int minDistance(string word1, string word2) {
-        int m=word1.length(),n=word2.length();
+        int n=word1.length(),m=word2.length();
         
-        vector<int> prev(n+1,0),cur(n+1,0);
+        // return recursion(n-1,m-1,word1,word2);
         
-        if(m==0 || n==0) return (m>0 ? m : n);
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        // Memoization
+        return memo(n,m,word1,word2,dp);
         
-        for(int i=0;i<=n;i++) prev[i]= i;
-        
-        for(int i=1;i<=m;i++)
-        {   
-            cur[0]=i;
-            for(int j=1;j<=n;j++)
-            {
-                if(word1[i-1]==word2[j-1])
-                {
-                    cur[j] = prev[j-1];
-                }
-                else
-                {
-                    cur[j] = min({prev[j],cur[j-1],prev[j-1]})+1;
-                }
-            }
-            prev = cur;
-        }
-         
-        return prev[n];
     }
 };
