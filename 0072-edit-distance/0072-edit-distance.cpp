cc80735257b1c110daec54acf6177aed436dc177
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-08-07 17:01:16 +0530

    Time: 10 ms (78.53%), Space: 6.6 MB (90.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -24,18 +24,8 @@ public:
                        memo(i-1,j-1,s1,s2,dp)});
     }
     
-    int minDistance(string word1, string word2) {
-        int n=word1.length(),m=word2.length();
-        
-        // return recursion(n-1,m-1,word1,word2);
-        
-        // vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
-        // Memoization
-        // return memo(n,m,word1,word2,dp);
-        
-        // Tabulation
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
-        
+    int tabulation(int n,int m,string &s1,string &s2,vector<vector<int>> &dp)
+    {
         for(int j=0;j<=m;j++) dp[0][j] = j;
         for(int i=0;i<=n;i++) dp[i][0] = i;
         
@@ -43,11 +33,41 @@ public:
         {
             for(int j=1;j<=m;j++)
             {
-                if(word1[i-1]==word2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                if(s1[i-1]==s2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
                 else dp[i][j] = 1+ min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
             }
         }
          
         return dp[n][m];
+    }
+    
+    int minDistance(string word1, string word2) {
+        int n=word1.length(),m=word2.length();
+        // return recursion(n-1,m-1,word1,word2);
+        
+        // vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        // Memoization
+        // return memo(n,m,word1,word2,dp);
+        
+        // Tabulation
+        // vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        // return tabulation(n,m,word1,word2,dp);
+        
+        vector<int> prev(m+1,0),cur(m+1,0);
+        
+        for(int j=0;j<=m;j++) prev[j] = j;
+        
+        for(int i=1;i<=n;i++)
+        {
+            cur[0]=i;
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1]) cur[j] = 0 + prev[j-1];
+                else cur[j] = 1+ min({cur[j-1], prev[j], prev[j-1]});
+            }
+            prev = cur;
+        }
+         
+        return prev[m];
     }
 };
