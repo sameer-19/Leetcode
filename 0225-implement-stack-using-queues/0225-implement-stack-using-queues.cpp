07d1ca8e6899a8ee8da8f3105a8aa26fb2acb584
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-08-28 23:20:13 +0530

    Time: 3 ms (36.88%), Space: 6.8 MB (25.78%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -6,44 +6,31 @@ public:
     }
     
     void push(int x) {
+        if(q1.empty())
+        {
+            q1.push(x);
+            return;
+        }
+        
+        int sz = q1.size(), i = 0;
         q1.push(x);
+        while(i<sz)
+        {
+            int top = q1.front();
+            q1.pop();
+            q1.push(top);
+            i++;
+        }
     }
     
     int pop() {
-        while(q1.size()>1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
         int top = q1.front();
         q1.pop();
-        
-        while(!q2.empty())
-        {
-            q1.push(q2.front());
-            q2.pop();
-        }
-        
         return top;
     }
     
     int top() {
-        while(q1.size()>1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        int top = q1.front();
-        q1.pop();
-        q2.push(top);
-        
-        while(!q2.empty())
-        {
-            q1.push(q2.front());
-            q2.pop();
-        }
-        
-        return top;
+        return q1.front();
     }
     
     bool empty() {
