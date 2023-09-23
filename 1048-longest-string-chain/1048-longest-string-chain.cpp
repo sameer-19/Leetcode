912232e666cf2096db49bc8a8c438e7c1fcd671d
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2023-09-23 23:00:52 +0530

    Time: 91 ms (56.33%), Space: 13.8 MB (55.67%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,19 +1,55 @@
 class Solution {
 public:
+    static bool cmp(string &a,string &b)
+    {
+        if(a.length()<b.length()) return true;
+        else if(a.length()==b.length()) return a<b;
+        
+        return false;
+    }
+    
+    bool compare(string &prev,string &cur)
+    {
+        int m=prev.length(), n = cur.length();
+        
+        if(n-m!=1) return false;
+        
+        int i = 0, j = 0;
+        
+        while(j<n)
+        {
+            if(prev[i]==cur[j]) {i++; j++;}
+            else
+            {
+                j++;
+            }
+        }
+        
+        if(i==m and j==n) return true;
+        return false;
+    }
+    
+    
+    
     int longestStrChain(vector<string>& words) {
-        unordered_map<string, int> dp;
-        int res = 1;
-        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
-        for (string word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); i++) {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev) != dp.end()) {
-                    dp[word] = dp[prev] + 1;
-                    res = max(res, dp[word]);
+        sort(words.begin(),words.end(),cmp);
+        
+        int n = words.size(), ans = 1;
+        
+        vector<int> dp(n,1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int prev_idx = 0;prev_idx<i;prev_idx++)
+            {
+                if(compare(words[prev_idx],words[i]))
+                {
+                    dp[i] = max(dp[i],1+dp[prev_idx]);
+                    ans = max(ans,dp[i]);
                 }
             }
         }
-        return res;
+        
+        return ans;
     }
 };
