3fa538f456b3d590cd96d3af7c24daebe2416eb2
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2024-11-24 17:50:26 +0530

    Time: 7 ms (11.44%), Space: 38.3 MB (87.50%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int countOfNegatives = 0, mn = 1e7;
+        long long ans = 0;
+        
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                countOfNegatives += matrix[i][j] < 0;
+                mn= min(mn, abs(matrix[i][j]));
+            }
+        }
+            
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                ans+=abs(matrix[i][j]);
+            }
+        }
+        
+        if(countOfNegatives%2==0)
+        {
+            return ans;
+        }
+        
+        return ans - (2*mn);
+    }
+};
