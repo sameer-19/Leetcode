faa48f3c265f082d8687199f78b8ef23ee4a035d
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2024-11-27 22:48:34 +0530

    Time: 113 ms (68.57%), Space: 115.5 MB (63.09%) - LeetHub

---
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+    public:
+        int bfs(vector<int> adj[], int n)
+        {
+            queue<int> q;
+            q.push(0);
+            int distance = 0;
+
+            vector<bool> visited(n, false);
+            visited[0] = true;
+
+            while (!q.empty())
+            {
+                int size = q.size();
+
+                for (int i = 0; i < size; i++)
+                {
+                    int node = q.front();
+                    q.pop();
+                    for (auto x: adj[node])
+                    {
+                        if (x == n - 1) return distance + 1;
+                        if (!visited[x])
+                        {
+                            visited[x] = true;
+                            q.push(x);
+                        }
+                    }
+                }
+                distance++;
+            }
+
+            return distance;
+        }
+
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector < int>> &queries)
+    {
+        vector<int> ans;
+
+        vector<int> adj[n];
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            adj[i].push_back(i + 1);
+        }
+
+        for (auto x: queries)
+        {
+            adj[x[0]].push_back(x[1]);
+            ans.push_back(bfs(adj, n));
+        }
+
+        return ans;
+    }
+};
