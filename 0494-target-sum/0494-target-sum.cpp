7c67af8daa76fe03faf0bdda45f23e09b3dd2753
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2024-12-26 10:54:23 +0530

    Time: 15 ms (59.97%), Space: 25.8 MB (26.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    void solve(int idx, int sum, vector<int> &nums, int &target, int &ans)
+    int totalSum;
+    int solve(int idx, int sum, vector<int> &nums, int &target, vector<vector<int>> &dp)
     {
         if(idx==nums.size())
         {
-            if(target==sum) ans++;
-            return;
+            if(target==sum) return 1;
+            return 0;
         }
         
-        solve(idx+1, sum+nums[idx], nums, target, ans);
-        solve(idx+1, sum-nums[idx], nums, target, ans);
+        if(dp[idx][totalSum+sum]!=INT_MIN) return dp[idx][totalSum+sum];
         
+        int add = solve(idx+1, sum+nums[idx], nums, target, dp);
+        int sub = solve(idx+1, sum-nums[idx], nums, target, dp);
+        
+        return dp[idx][totalSum+sum] = add + sub;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = 0;
-        solve(0,0,nums, target, ans);
+        int n = nums.size();
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
         
-        return ans;
+        vector<vector<int>> dp(n, vector<int> (2*totalSum + 1, INT_MIN));
+        return solve(0,0,nums, target, dp);
     }
 };
